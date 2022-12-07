bda0052273bf8daf1a2dbad0ada036ba0e46f9c2
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-07 14:12:48 -0300

    🚧 feat: implementou exercício de quantidade de raízes reais

--- funcoes2/ex6.py
+++ funcoes2/ex6.py
@@ -3,6 +3,19 @@ def raizes(a, b, c):
     delta = (b**2) - (4 * a * c) 
  
     print(delta)
+    if(delta < 0):
+        print("Não há raízes reais!")
+
+    elif (delta == 0):
+
+       result = b / (2 * a)
+
+       print(f"Há {result} raízes reais")
+    else:
+        print("Possui duas raízes reais!")
+
 
 
 raizes(6,11,-35)
+raizes(2,4,2)
+raizes(-4,-7,-12)
