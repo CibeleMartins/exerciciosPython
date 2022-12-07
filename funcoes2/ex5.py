be46c6f634ddd114b6fb2dadba266db408e4d9a2
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-07 11:19:11 -0300

    🚧 feat: implementando exercício de progressão aritmética

---
+++ funcoes2/ex5.py
@@ -0,0 +1,20 @@
+def n_termo(i, r, n):
+
+    inicial = i
+    razao = r
+    numero_elementos = n
+
+    lista_result = []
+
+    for pa in range(inicial, numero_elementos, razao): 
+
+        result = pa + inicial
+
+        
+
+    return lista_result
+
+     
+
+
+print(n_termo(100,-1,100))
