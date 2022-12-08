c0dbfb77a451fd658a2a5fb1f78240d6c954cf53
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-08 14:24:15 -0300

    🚧 feat: implementou exercício de calcular média de salários

--- exercicios-07-12/ex2.py
+++ exercicios-07-12/ex2.py
@@ -1,28 +1,16 @@
 def salarioMedioSatisfacaoGarantida():
 
-    # colaborador = ''
-    # salario = ''
+    listaColaboradores = []
 
-    # colaborador, salario = map(str ,input("Digite o seu nome de colaborador e o seu salário separados por vírgula: ").split())
-
-    # if(colaborador == "Fim" and salario):
-
-    #     print(f"{colaborador}, {salario:.2f}")
-    
-    # salarioMedioSatisfacaoGarantida()
-
-    colaborador_salario = input("Digite o seu nome e salário:  ").split()
-
-    if("Fim" in colaborador_salario):
+    for i in range(0, 10):
+        nome, salario = input("Digite seu nome e salário: ").split()
         
-        map(lambda i: print(i), colaborador_salario)
-    else:
-        salarioMedioSatisfacaoGarantida()
 
+        listaColaboradores.append(nome)
+        listaColaboradores.append(salario)
+
+    print(listaColaboradores)
   
-
-    
-
 
 
    
