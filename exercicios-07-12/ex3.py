5dba6eae539480690e55168b3b9ef1439839d808
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-12 10:37:26 -0300

    🚧 feat: implementou exercício de maior salário

--- exercicios-07-12/ex3.py
+++ exercicios-07-12/ex3.py
@@ -1,3 +1,5 @@
+
+
 def maiorSalario():
 
     listaColaboradores = []
@@ -14,11 +16,14 @@ def maiorSalario():
 
     for salario in range(0 + 1, len(listaColaboradores), 2):
         # print(salario)
-        print(listaColaboradores[salario])
+        # print(listaColaboradores[salario])
 
         listaSalarios.append(listaColaboradores[salario])
     
-    map(lambda i: )
 
+    salarios_float = map(lambda i: float(i), listaSalarios)
+    
+    for salario_numero in salarios_float:
+        print(f"O maior salário é:", max(salarios_float))
 
 maiorSalario()
