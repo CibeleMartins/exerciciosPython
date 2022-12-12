ecefb898b9ff1a3375bc95b4ac3d4854a46c1280
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-12 11:44:19 -0300

    🚧 feat: implementou exercício 4

--- exercicios-07-12/ex4.py
+++ exercicios-07-12/ex4.py
@@ -1,10 +1,34 @@
-
+import functools
 def maiorMenorMedia():
 
     n = int(input("Digite um número: "))
 
-    count = 0
+    listaColaboradores = []
+    listaSalarios = []
 
-    if count < n:
+    for i in range(0, n):
 
-      nome, salario = input("Digite seu nome e seu salário: ").strip().split()
+        # nome, salario = input("Digite seu nome e seu salário: ").split()
+
+        nome = input("Digite seu nome: ")
+        salario = input("Digite seu salário: ")
+
+        juncao_nome_salario = nome + ', ' + salario
+       
+        listaColaboradores.append(juncao_nome_salario)
+        listaSalarios.append(float(salario))
+        # listaColaboradores.append(salario)
+    
+    soma_salarios = functools.reduce(lambda a, p: a + p)
+
+    media_salarial = soma_salarios / len(listaSalarios)
+
+    print(*listaColaboradores, sep="\n")
+
+
+
+
+
+
+
+maiorMenorMedia()
