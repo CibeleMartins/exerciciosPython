6ad07c8d99297c51ec8b484e2bd4e524f7a7b4be
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-12 15:31:08 -0300

    🚧 feat: implementou exercício 6

--- exercicios-07-12/ex6.py
+++ exercicios-07-12/ex6.py
@@ -7,8 +7,24 @@
 def aAlcancaraBEm():
 
     populacao_a, populacao_b = map(int, input("Digite a populacao da ciadade A e B: ").split())
-    taxa_cresc_a, taxa_cresc_b = input("Digite a taxa -> \"%\" de crescimento anual de A e B: ").split()
+    taxa_cresc_a, taxa_cresc_b = input("Digite a taxa de crescimento anual de A e B: ").split()
 
-    print(populacao_a, taxa_cresc_a, populacao_b, taxa_cresc_b)
+    anos = 0
+
+    while(populacao_a < populacao_b):
+
+        anos += 1
+
+        populacao_a = populacao_a + (populacao_a * float(taxa_cresc_a))
+        populacao_b - populacao_a + (populacao_b * float(taxa_cresc_b))
+    
+    print(f"Após {anos} ano(s) cidade A alcançará a B em população.")
+    print(f"Cidade A: %.0f" %(populacao_a))
+    print(f"Cidade B: %.0f" %(populacao_b))
+
+
+
+
+    
 
 aAlcancaraBEm()
