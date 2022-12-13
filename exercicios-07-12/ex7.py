21f645555c69762fd4111f95f0f2890f06d75b1a
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-13 13:13:36 -0300

    🚧 feat: implementou exercício fibonacci

--- exercicios-07-12/ex7.py
+++ exercicios-07-12/ex7.py
@@ -1,7 +1,8 @@
-# fibonacci sequencia de numeros inteiros
-# comeca por 0
-# cada termo subsequente corresponde a soma dos dois anteriores
+# fibonacci sequencia de numeros inteiros ok
+# comeca por 0 ok
 # dois prmeiros valores da sequencia 0 e 1
+# cada termo subsequente corresponde a soma dos dois anteriores
+
 
 # def fibonacci(n):
 
@@ -10,25 +11,31 @@
 
 #     if n == 2:
 #         return 1
-    
+
 #     print(n)
 #     return fibonacci(n - 1) + fibonacci(n - 2) - 1
 
-    
+
 # print(fibonacci(1))
 
 def fibonacci(n):
 
-    termo_subsequente = 0
-
     for i in range(0, n):
 
         if i == 0:
-           print(0)
+            print(0)
         if i == 1:
             print(1)
 
-    print((i - 1 ) + (i - 1))
+        if i > 1:
 
-       
-fibonacci(2)
+            fibo = i - 1
+            nacci = i - 2
+
+            termo = fibo + nacci
+
+            print(termo)
+ 
+
+
+fibonacci(10)
