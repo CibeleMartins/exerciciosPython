d88583f9994d865c095e44743e62251d3d713527
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-14 15:27:56 -0300

    🚧 feat: implementou exercício 10

--- exercicios-07-12/ex10.py
+++ exercicios-07-12/ex10.py
@@ -6,31 +6,37 @@ def corrigeFrase():
 
     frase = input("Digite uma frase: ")
 
-    # index_9 = frase.index("9")
-
-    # index_6 = frase.index("6")
-
-    # index_5 = frase.index("5")
-
-    # index_7 = frase.index("7")
-
-    # index_0 = frase.index("0")
-
-    # index_1 = frase.index("1")
-
-
-
     if "9" in frase:
 
         nova_frase = frase.replace("9", "p")
 
-        print(nova_frase)
+    print(nova_frase)
 
     if "6" in nova_frase:
 
         nova_frase2 = nova_frase.replace("6", "b")
 
-        print(nova_frase2)
+    print(nova_frase2)
+
+    if "5" in nova_frase2:
+
+        nova_frase3 = nova_frase2.replace("5", "s")
+
+    print(nova_frase3)
+
+    if "7" in nova_frase3:
+
+        nova_frase4 = nova_frase3.replace("7", "t")
+    
+    if "0" in nova_frase4:
+
+        nova_frase5 = nova_frase4.replace("0", "o")
+
+    if "1" in nova_frase5:
+
+        nova_frase6 = nova_frase5.replace("1", "i")
+
+    print(nova_frase6)
 
      
         
@@ -40,42 +46,7 @@ def corrigeFrase():
 
       
 
-    # if "6" in frase:
 
-    #     nova_frase = frase.replace("6", "b")
-
-    #     print(nova_frase)
-
-    # if "5" in frase:
-
-    #     nova_frase = frase.replace("5", "s")
-
-    #     print(nova_frase)
-
-    # if "7" in frase:
-
-    #     nova_frase = frase.replace("7", "t")
-
-    #     print(nova_frase)
-
-    # if "0" in frase:
-
-    #     nova_frase = frase.replace("0", "o")
-
-    #     print(nova_frase)
-    
-    # if "1" in frase:
-
-    #     nova_frase = frase.replace("1", "i")
-
-    #     print(nova_frase)
-
-        
-        
-    # if "6" in frase:
-
-    #     index_seis = frase.index("6")
-    #     frase[index_seis] = "b"
    
 
 corrigeFrase()
