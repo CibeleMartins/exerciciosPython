5f433697138e9f9e6c7076937d8a939979cb7305
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-15 10:20:23 -0300

    🚧 feat: implementou exercício 10

--- exercicios-07-12/ex10.py
+++ exercicios-07-12/ex10.py
@@ -8,45 +8,17 @@ def corrigeFrase():
 
     if "9" in frase:
 
-        nova_frase = frase.replace("9", "p")
-
-    print(nova_frase)
-
-    if "6" in nova_frase:
-
-        nova_frase2 = nova_frase.replace("6", "b")
-
-    print(nova_frase2)
-
-    if "5" in nova_frase2:
-
-        nova_frase3 = nova_frase2.replace("5", "s")
-
-    print(nova_frase3)
-
-    if "7" in nova_frase3:
-
-        nova_frase4 = nova_frase3.replace("7", "t")
+       nova_frase = frase.replace("9", "p")
+       print(nova_frase)
     
-    if "0" in nova_frase4:
+    if "6" in nova_frase or "6" in frase:
 
-        nova_frase5 = nova_frase4.replace("0", "o")
+        nova_frase2 = nova_frase.replace("6", "b") or frase.replace("6", "b")
+        print(nova_frase2)  
 
-    if "1" in nova_frase5:
+    if "5" in nova_frase2 or "5" in frase or "5" in nova_frase:
 
-        nova_frase6 = nova_frase5.replace("1", "i")
-
-    print(nova_frase6)
-
-     
-        
-    
-
-
-
-      
-
-
+        nova_frase3 = nova_frase2.replace("5", "s") or nova_frase.replace("5", "s") or frase.replace("5", "s")
+        print(nova_frase3)
    
-
 corrigeFrase()
