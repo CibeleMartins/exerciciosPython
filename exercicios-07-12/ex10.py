b35c66bf732b85f99cbd961619fae3f446fb6915
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-16 10:25:44 -0300

    🚧 feat: implementou exercício 10

--- exercicios-07-12/ex10.py
+++ exercicios-07-12/ex10.py
@@ -1,24 +1,17 @@
-#  uma fra sem numeros
-# com letr maiuscula apenas no inicio
-# com ponto final
-
 def corrigeFrase():
 
     frase = input("Digite uma frase: ")
 
-    if "9" in frase:
+    substituicao_numeros = frase.replace("9", "p").replace("6", "b").replace(
+        "5", "s").replace("7", "t").replace("0", "o").replace("1", "i")
 
-       nova_frase = frase.replace("9", "p")
-       print(nova_frase)
-    
-    if "6" in nova_frase or "6" in frase:
+    frase_corrigida = substituicao_numeros.capitalize()
 
-        nova_frase2 = nova_frase.replace("6", "b") or frase.replace("6", "b")
-        print(nova_frase2)  
+    if frase_corrigida.endswith('.'):
+        print(True)
+        print(frase_corrigida)
+    else:
+        print(frase_corrigida + '.')
 
-    if "5" in nova_frase2 or "5" in frase or "5" in nova_frase:
 
-        nova_frase3 = nova_frase2.replace("5", "s") or nova_frase.replace("5", "s") or frase.replace("5", "s")
-        print(nova_frase3)
-   
 corrigeFrase()
---
+++ exercicios-07-12/ex11.py
@@ -0,0 +1,26 @@
+# 0 jogador corinthians
+#  1 jogador sao paulo
+
+
+print('-'*30)
+print('Anote a posição dos jogadores. 1 para jogador do São Paulo e 0 para jogador do Corinthians')
+print('-'*30)
+
+posicoes_jogadores = input("Digite as posições: ")
+
+
+for i in range(0, len(posicoes_jogadores) + 1):
+
+    print(posicoes_jogadores[i:3])
+
+
+
+
+
+
+
+
+
+
+
+
