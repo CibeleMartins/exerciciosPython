62eccbfda80d531aeb0991e9011fe9a9aafd13d5
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-16 10:32:04 -0300

    🚧 feat: implementou exercício 11

--- exercicios-07-12/ex1.py
+++ exercicios-07-12/ex1.py
@@ -1,28 +1,4 @@
 
-# def nao_multiplo3(n):
-#     if n == 0:
-#         return print(False)
-#     elif n == 1 or n == 2:
-#         return print(True)
-#     else:
-#         return multiplo3(n-3)
-
-
-# def multiplo3(n):
-
-#     if n == 0:
-#         return print(True)
-#     elif n == 1 or n == 2:
-#         return  print(False)
-#     else:
-#         nao_multiplo3(n-3)
-
-
-# # multiplo3(12)
-# # nao_multiplo3(12)
-# multiplo3(15)
-
-
 def nao_multiplo3(n):
  
     if n < 0 or n > 0:
--- exercicios-07-12/ex11.py
+++ exercicios-07-12/ex11.py
@@ -1,17 +1,18 @@
-# 0 jogador corinthians
-#  1 jogador sao paulo
-
-
 print('-'*30)
 print('Anote a posição dos jogadores. 1 para jogador do São Paulo e 0 para jogador do Corinthians')
 print('-'*30)
 
 posicoes_jogadores = input("Digite as posições: ")
 
+if "1111111" in posicoes_jogadores:
+    print("VAMO TRICOLOR")
 
-for i in range(0, len(posicoes_jogadores) + 1):
+elif "0000000" in posicoes_jogadores:
+    print("VAI TIMAO")
 
-    print(posicoes_jogadores[i:3])
+else:
+    print("BORA UM VIRTUAL NO CODEFORCES")
+
 
 
 
