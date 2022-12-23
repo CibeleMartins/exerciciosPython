b3cb293fded88d146066edb805299ba3dc1af10b
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-23 15:27:20 -0300

    ⚰️ feat: removeu importação de modulos de cores

--- desafioResistores/desafioResistores.py
+++ desafioResistores/desafioResistores.py
@@ -1,43 +1,34 @@
 import streamlit as st
 
-
-from colorama import init
-from termcolor import colored
-
 import IEC60062 as mostrar_cores
 
 st.set_page_config(page_title="Cores resistores!", layout='centered')
 
 with st.container():
 
-    # with open('style.css') as style:
-
-    #     st.markdown(f'<style>{style.read()}</style>', unsafe_allow_html=True)
-
     st.header("Desafio de resistores!")
     st.text("Receba o valor de uma resistencia e a traduza para suas respectivas cores.")
 
-    valor_resistencia = st.text_input(label="Digite o valor da resistencia",value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible")
+    valor_resistencia = st.text_input(label="Digite o valor da resistencia", value="", max_chars=None, key=None, type="default", help=None,
+                                      autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible")
 
     button = st.button(label="Ver cores")
 
-   
-
-
 dict_cores = {
-        'Preto': '#000000',
-        'Marrom': '#8B4513',
-        'Vermelho': '#FF0000',
-        'Laranja': '#FFA500',
-        'Amarelo': '#FFFF00',
-        'Verde': '#008000',
-        'Azul': '	#0000FF',
-        'Violeta': '#A020F0',
-        'Cinza': '#808080',
-        'Branco': '#FFFFFF'
+    'Preto': '#000000',
+    'Marrom': '#8B4513',
+    'Vermelho': '#FF0000',
+    'Laranja': '#FFA500',
+    'Amarelo': '#FFFF00',
+    'Verde': '#008000',
+    'Azul': '	#0000FF',
+    'Violeta': '#A020F0',
+    'Cinza': '#808080',
+    'Branco': '#FFFFFF',
+    'Rosa': '#FF1493',
+    'Prata': '#C0C0C0',
+    'Dourado': '#FFD700'
 }
-
-lista = []
 
 if button and len(valor_resistencia) > 0:
 
@@ -45,18 +36,8 @@ if button and len(valor_resistencia) > 0:
 
     lista_cores = mostrar_cores.IEC60062(valor_resistencia).copy()
     count = 0
+
     for i in lista_cores:
 
-      count += 1  
-      st.color_picker(i, dict_cores[i], key=count)
-
-    
-
-
-
-
-
-
-
-
-
+        count += 1
+        st.color_picker(i, dict_cores[i], key=count)
